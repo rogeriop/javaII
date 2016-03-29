e5e7c2d16eec9b274825b9be04ee673a20dbb1cf
rogeriop <pinheiroxrogerio@gmail.com>
2016-03-29 14:07:33 -0300

    fim 06 - Excecoes e controles de erros

--- src/Conta.java
+++ src/Conta.java
@@ -6,8 +6,12 @@ abstract class Conta {
         return this.saldo;
     }
 
-    public void deposita(double valor) {
-        this.saldo += valor;
+    public void deposita(double valor) throws ValorInvalidoException {
+        if (valor < 0) {
+            throw new ValorInvalidoException(valor);
+        } else {
+            this.saldo += valor  - 0.10;        
+        }        
     }
 
     public void saca(double valor) {
